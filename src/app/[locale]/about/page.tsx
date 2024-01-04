eb39b1d9fcd36c8008e4205c31f5b89f42300654
Nrgiz Aliyeva <nara0120>
2024-01-04 16:10:13 +0400

    Add policy and about

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,17 +1,36 @@
-import React from 'react'
-import { useTranslations } from 'next-intl'
-
+import React from "react";
+import { useTranslations } from "next-intl";
+import { Text, Box, Heading } from "@chakra-ui/react";
 
 const About = () => {
-    const t = useTranslations()
+  const t = useTranslations();
   return (
     <>
-    <div>About</div>
-    <h1>{t('About.mission.title')}</h1>
-    
+      <Box>
+        <Box
+          display="flex"
+          flexDirection="column"
+          justifyContent="start"
+          alignItems="left"
+        >
+          <Heading>{t("About.mission.title")}</Heading>
+          <Text>{t("About.mission.body")}</Text>
+          <Text>{/* <h1>{t("About.body.subtitle")}</h1> */}</Text>
+        </Box>
+        <Box>
+          <Heading>{t("About.value.title")}</Heading>
+          <Text> {t("About.value.body.transparent.subtitle")}</Text>
+          <Text> {t("About.value.body.transparent.caption")}</Text>
+          <Text>{t("About.value.body.innovative.subtitle")}</Text>
+          <Text>{t("About.value.body.innovative.caption")}</Text>
+          <Text>{t("About.value.body.team.subtitle")}</Text>
+          <Text>{t("About.value.body.team.caption")}</Text>
+          <Text>{t("About.value.body.tenacity.subtitle")}</Text>
+          <Text>{t("About.value.body.tenacity.caption")}</Text>
+        </Box>
+      </Box>
     </>
+  );
+};
 
-  )
-}
-
-export default About
+export default About;
