af51debe763b881f0d198dff95d70aa68b237f99
Nrgiz Aliyeva <nara0120>
2024-01-05 17:37:40 +0400

    public branch

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useTranslations } from "next-intl";
-import { Text, Box, Heading } from "@chakra-ui/react";
+import { Text, Box, Heading, Container } from "@chakra-ui/react";
 
 const About = () => {
   const t = useTranslations();
@@ -10,24 +10,49 @@ const About = () => {
         <Box
           display="flex"
           flexDirection="column"
-          justifyContent="start"
+          justifyContent="center"
+          pl="30px"
+          ml="30px"
+          mt="30px"
           alignItems="left"
+          // backgroundImage="url('/public/mission(2).svg')"
         >
           <Heading>{t("About.mission.title")}</Heading>
-          <Text>{t("About.mission.body")}</Text>
+
+          <Text mg="5px">{t("About.mission.body")}</Text>
           <Text>{/* <h1>{t("About.body.subtitle")}</h1> */}</Text>
         </Box>
-        <Box>
-          <Heading>{t("About.value.title")}</Heading>
-          <Text> {t("About.value.body.transparent.subtitle")}</Text>
-          <Text> {t("About.value.body.transparent.caption")}</Text>
-          <Text>{t("About.value.body.innovative.subtitle")}</Text>
-          <Text>{t("About.value.body.innovative.caption")}</Text>
-          <Text>{t("About.value.body.team.subtitle")}</Text>
-          <Text>{t("About.value.body.team.caption")}</Text>
-          <Text>{t("About.value.body.tenacity.subtitle")}</Text>
-          <Text>{t("About.value.body.tenacity.caption")}</Text>
-        </Box>
+        <Container bgImage="url(public/mission(2).svg)">
+          <Box margin="10px">
+            <Heading>{t("About.value.title")}</Heading>
+          </Box>
+          <Box margin="10px">
+            <Heading as="h4" size="md">
+              {t("About.value.body.transparent.subtitle")}
+            </Heading>
+
+            <Text> {t("About.value.body.transparent.caption")}</Text>
+          </Box>
+          <Box>
+            <Heading as="h4" size="md">
+              {t("About.value.body.innovative.subtitle")}
+            </Heading>
+
+            <Text>{t("About.value.body.innovative.caption")}</Text>
+          </Box>
+          <Box>
+            <Heading as="h4" size="md">
+              {t("About.value.body.team.subtitle")}
+            </Heading>
+            <Text>{t("About.value.body.team.caption")}</Text>
+          </Box>
+          <Box>
+            <Heading as="h4" size="md">
+              {t("About.value.body.tenacity.subtitle")}
+            </Heading>
+            <Text>{t("About.value.body.tenacity.caption")}</Text>
+          </Box>
+        </Container>
       </Box>
     </>
   );
