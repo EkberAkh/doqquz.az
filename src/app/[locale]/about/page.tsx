aeab41e668173131444811edb8289b25e11a8013
Nrgiz Aliyeva <nara0120>
2024-01-07 21:23:58 +0400

    started about page

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -6,56 +6,88 @@ const About = () => {
   const t = useTranslations();
   return (
     <>
-      <Box padding='20px' >
-        <Box gap='20px' backgroundColor='white'  boxShadow='0 2px 8px rgba(0,0,0,.08)' minH="384px" display="flex" >
-          <Box padding='20px' backgroundImage="url('../../../../images/underline.png')"
+      <Box padding="20px">
+        <Box
+          gap="20px"
+          backgroundColor="white"
+          boxShadow="0 2px 8px rgba(0,0,0,.08)"
+          minH="384px"
+          display="flex"
+        >
+          <Box
+            padding="20px"
+            backgroundImage="url('../../../../images/underline.png')"
             backgroundSize="contain"
-            backgroundRepeat="no-repeat">
-            <Heading as='h2'>Missiyamiz</Heading>
-            <Text width='400px'>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat odit quidem totam ipsam mollitia voluptatem corporis eius magnam vel? Ratione delectus eos quaerat earum, doloremque esse eaque officiis perspiciatis labore.
+            backgroundRepeat="no-repeat"
+          >
+            <Heading as="h2">{t("About.value.title")}</Heading>
+            <Text width="400px">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat
+              odit quidem totam ipsam mollitia voluptatem corporis eius magnam
+              vel? Ratione delectus eos quaerat earum, doloremque esse eaque
+              officiis perspiciatis labore.
             </Text>
-
           </Box>
           <Box
-          flex='1'
+            flex="1"
             backgroundImage="url('../../../../images/mission.png')"
             backgroundSize="cover"
             backgroundRepeat="no-repeat"
-            backgroundPosition='center'
+            backgroundPosition="center"
           ></Box>
         </Box>
-        <Container bgImage="url(public/mission(2).svg)">
-          <Box margin="10px">
-            <Heading>{t("About.value.title")}</Heading>
-          </Box>
-          <Box margin="10px">
-            <Heading as="h4" size="md">
-              {t("About.value.body.transparent.subtitle")}
-            </Heading>
+      </Box>
+      <Box padding="20px">
+        <Box
+          backgroundImage="url('../../../../images/panel-bg.png')"
+          gap="20px"
+          backgroundColor="white"
+          boxShadow="0 2px 8px rgba(0,0,0,.08)"
+          minH="384px"
+          display="flex"
+        >
+          <Box
+            padding="20px"
+            backgroundImage="url('../../../../images/underline.png')"
+            backgroundSize="contain"
+            backgroundRepeat="no-repeat"
+          >
+            {/* <Heading as="h2">{t("About.value.title")}</Heading> */}
+            <Box>
+              <Text width="400px">
+                <Heading as="h2">{t("About.value.title")}</Heading>
+                <Text width="400px"></Text>
+                <Box>{t("About.value.body.transparent.caption")}</Box>
+                <Text> {t("About.value.body.transparent.caption")} </Text>
+              </Text>
+            </Box>
+            <Box
+              display="flex"
+              flexDirection="column"
+              flex="1"
+              backgroundImage="url('../../../../images/panel-bg.png')"
+            ></Box>
+            <Box>
+              <Heading as="h4" size="md">
+                {t("About.value.body.innovative.subtitle")}
+              </Heading>
 
-            <Text> {t("About.value.body.transparent.caption")}</Text>
+              <Text>{t("About.value.body.innovative.caption")}</Text>
+            </Box>
+            <Box>
+              <Heading as="h4" size="md">
+                {t("About.value.body.team.subtitle")}
+              </Heading>
+              <Text>{t("About.value.body.team.caption")}</Text>
+            </Box>
+            <Box>
+              <Heading as="h4" size="md">
+                {t("About.value.body.tenacity.subtitle")}
+              </Heading>
+              <Text>{t("About.value.body.tenacity.caption")}</Text>
+            </Box>
           </Box>
-          <Box>
-            <Heading as="h4" size="md">
-              {t("About.value.body.innovative.subtitle")}
-            </Heading>
-
-            <Text>{t("About.value.body.innovative.caption")}</Text>
-          </Box>
-          <Box>
-            <Heading as="h4" size="md">
-              {t("About.value.body.team.subtitle")}
-            </Heading>
-            <Text>{t("About.value.body.team.caption")}</Text>
-          </Box>
-          <Box>
-            <Heading as="h4" size="md">
-              {t("About.value.body.tenacity.subtitle")}
-            </Heading>
-            <Text>{t("About.value.body.tenacity.caption")}</Text>
-          </Box>
-        </Container>
+        </Box>
       </Box>
     </>
   );
