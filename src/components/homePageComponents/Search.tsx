890abbca863dfc36b04d5845e1d6989a5e086776
RaminEmrahov <ramin.emrahov77@mail.ru>
2024-01-11 15:03:13 +0400

    Header Searching styling

--- src/components/homePageComponents/Search.tsx
+++ src/components/homePageComponents/Search.tsx
@@ -1,20 +1,21 @@
 'use client'
 import { colorObjects } from "@/consts"
 import { SearchIcon } from "@chakra-ui/icons"
-import { Flex, Box, Button, InputGroup, Input, InputLeftElement, Icon } from "@chakra-ui/react"
+import { Flex, Box, Button, InputGroup, Input, InputLeftElement, Icon, InputRightElement } from "@chakra-ui/react"
 import { useTranslations } from "next-intl"
 
 export const Search = () => {
     const t = useTranslations()
     return (
-        <Flex width='100%' mt='100px'>
+        <Flex width='100%' mt='100px' w={'100%'} justify={'center'}>
+            <Flex width={'90%'} >
             <Box w='45%'>
                 <Button variant='primary' mb='20px' bg={'#2a41e8'} color={'#fff'} fontWeight='400' cursor='auto'>
                     {t('Home.Header.questions.where')}
                 </Button>
 
                 <InputGroup bgColor={colorObjects.white.main} w='100%' p='10px 12px'
-                    boxShadow='0 2px 4px rgba(0, 0, 0, 0.1)' borderLeftRadius='10px' borderRight='1px solid #e4e4e4'>
+                    boxShadow='0 2px 4px rgba(0, 0, 0, 0.1)' borderLeftRadius='5px' borderRight='1px solid #e4e4e4'>
                     <Input placeholder={t('Common.location')} fontSize='1.2rem' outline='none' border='none' />
                 </InputGroup>
 
@@ -26,12 +27,29 @@ export const Search = () => {
                 </Button>
                 <Box width='100%'>
                     <InputGroup bgColor={colorObjects.white.main} w='100%' p='10px 12px'
-                        boxShadow='0 2px 4px rgba(0, 0, 0, 0.1)' borderRightRadius='10px' >
+                        boxShadow='0 2px 4px rgba(0, 0, 0, 0.1)' borderRightRadius='5px' >
                         <Input placeholder={t('Home.Header.placeholders.jobType')} fontSize='1.2rem' outline='none' border='none' />
+                        <InputRightElement width='8rem' h={'100%'} >
+                                <Button
+                                    // pr={'10px'}
+                                    w='100%'
+                                    h='3rem'
+                                    mr={'10px'}
+                                    fontSize='14px'
+                                    borderRadius='4px'
+                                    color='#fff'
+                                    bg="#2a41e8"
+                                    boxShadow=" 0px 3px 1px -2px rgba(0,0,0,0.2), 0px 2px 2px 0px rgba(0,0,0,0.14), 0px 1px 5px 0px rgba(0,0,0,0.12)"
+                                    _hover={{ }}
+                                >
+                                    {t('Common.Action.SEARCH')}
+                                </Button>
+                            </InputRightElement>
                     </InputGroup>
 
                 </Box>
             </Box>
+            </Flex>
         </Flex >
     )
 }
