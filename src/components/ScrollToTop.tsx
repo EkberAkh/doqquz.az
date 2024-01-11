1322655075f6a9a6894ccadcf85827789adf8af1
RaminEmrahov <ramin.emrahov77@mail.ru>
2024-01-11 14:28:30 +0400

    Scroll style

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -2,40 +2,43 @@ import { ChevronUpIcon } from '@chakra-ui/icons';
 import { useEffect, useState } from 'react';
 
 export default function ScrollToTop() {
-    // const [isVisible, setIsVisible] = useState(false)
-    // useEffect(() => {
-    //     window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+  const [isVisible, setIsVisible] = useState(false);
 
-    //     const scrollY = window.scrollY;
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      setIsVisible(scrollPosition > 100);
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
-    //     const triggerPosition = 200;
-    //     if(scrollY > triggerPosition) {
-    //         setIsVisible(true)
-    //     }
-        
-    // }, [scrollY,setIsVisible]);
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+  };
 
-    return (
-        <div>
-
-            <button
-                onClick={() => {
-                    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
-                }}
-                style={{
-                    position: 'fixed',
-                    width: '2.5rem',
-                    height: '2.5rem',
-                    fontSize: '1.5rem',
-                    bottom: '1rem',
-                    right: '1rem',
-                    borderRadius: '100%',
-                    backgroundColor: '#2a41e8',
-                    color: '#fff',
-                }}
-            >
-                <ChevronUpIcon />
-            </button>
-        </div>
-    );
+  return (
+    <div>
+      {isVisible && (
+        <button
+          onClick={scrollToTop}
+          style={{
+            position: 'fixed',
+            width: '2.5rem',
+            height: '2.5rem',
+            fontSize: '1.5rem',
+            bottom: '1rem',
+            right: '1rem',
+            borderRadius: '100%',
+            backgroundColor: '#2a41e8',
+            color: '#fff',
+          }}
+        >
+          <ChevronUpIcon />
+        </button>
+      )}
+    </div>
+  );
 }
