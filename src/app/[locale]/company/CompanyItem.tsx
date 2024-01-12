ff1bc4c35644d6305c45c9da4e2a07c8eb51dc69
RaminEmrahov <ramin.emrahov77@mail.ru>
2024-01-12 20:05:08 +0400

    Request to Api for Company

--- src/app/[locale]/company/CompanyItem.tsx
+++ src/app/[locale]/company/CompanyItem.tsx
@@ -1,101 +1,45 @@
+'use client'
 import CompanyIcon from '@/icons/CompanyIcon'
-import { Card, CardBody, Center, Flex, Text } from '@chakra-ui/react'
+import { Card, CardBody, Center, Flex, Spinner, Text } from '@chakra-ui/react'
 import React from 'react'
 
-const CompanyItem = () => {
+const CompanyItem = ({companies}) => {
+    // const [isLoading, setIsLoading] = useState(false);
+
     return (
         <Flex gap={'1.875rem'} mt={'1.875rem'} flexWrap={'wrap'}>
 
-            <Card
-                align='center'
-                boxShadow={'0 2px 10px rgba(0,0,0,.1)'}
-                p='38px'
-                mb={'30px'}
-                width='calc(33.3% - 20px)'
-                cursor={'pointer'}
-                _hover={{ transform: 'translateY(-3px)' }}
-                transition={'0.3s'}>
-                <CardBody textAlign={'center'} alignItems={'center'} justifyContent={'center'}>
-                    <Flex
-                        align={'center'}
-                        justify={'center'}
-                        borderRadius='100px'
-                        width='70px'
-                        height='70px'
-                        bg='#bdbdbd'
-                        marginBottom={'30px'}
-                    >
-                        <CompanyIcon
-                            width='25'
-                            height='25'
-                            color='#fafafa'
-                        />
-                    </Flex>
-                    <Text mb={'0.5rem'} color={'#333'} fontWeight={'600'} fontSize={'1.125rem'}>Sirket MMC</Text>
-                </CardBody>
-            </Card>
+            {companies.map((company) =>
+                <Card
+                    key={company.id}
+                    align='center'
+                    boxShadow={'0 2px 10px rgba(0,0,0,.1)'}
+                    p='38px'
+                    mb={'30px'}
+                    width='calc(33.3% - 20px)'
+                    cursor={'pointer'}
+                    _hover={{ transform: 'translateY(-3px)' }}
+                    transition={'0.3s'}>
 
-            <Card
-                align='center'
-                boxShadow={'0 2px 10px rgba(0,0,0,.1)'}
-                p='38px'
-                mb={'30px'}
-                width='calc(33.3% - 20px)'
-                cursor={'pointer'}
-                _hover={{ transform: 'translateY(-3px)' }}
-                transition={'0.3s'}>
-                <CardBody textAlign={'center'} alignItems={'center'} justifyContent={'center'}>
-                    <Flex
-                        align={'center'}
-                        justify={'center'}
-                        borderRadius='100px'
-                        width='70px'
-                        height='70px'
-                        bg='#bdbdbd'
-                        marginBottom={'30px'}
-                    >
-                        <CompanyIcon
-                            width='25'
-                            height='25'
-                            color='#fafafa'
-                        />
-                    </Flex>
-                    <Text mb={'0.5rem'} color={'#333'} fontWeight={'600'} fontSize={'1.125rem'}>Sirket MMC</Text>
-                </CardBody>
-            </Card>
-
-            <Card
-                align='center'
-                boxShadow={'0 2px 10px rgba(0,0,0,.1)'}
-                p='38px'
-                mb={'30px'}
-                width='calc(33.3% - 20px)'
-                cursor={'pointer'}
-                _hover={{ transform: 'translateY(-3px)' }}
-                transition={'0.3s'}>
-
-                <CardBody >
-                    <Flex
-                        align={'center'}
-                        justifyContent={'center'}
-                        textAlign={'center'}
-                        borderRadius='100px'
-                        width='70px'
-                        height='70px'
-                        bg='#bdbdbd'
-                        marginBottom={'30px'}
-                    >
-                        <CompanyIcon
-                            width='25'
-                            height='25'
-                            color='#fafafa'
-                        />
-                    </Flex>
-                    <Text mb={'0.5rem'} color={'#333'} fontWeight={'600'} fontSize={'1.125rem'}>Sirket MMC</Text>
-                </CardBody>
-            </Card>
-
-
+                    <CardBody textAlign={'center'} alignItems={'center'} justifyContent={'center'}>
+                        <Flex
+                            align={'center'}
+                            justify={'center'}
+                            borderRadius='100px'
+                            width='70px'
+                            height='70px'
+                            bg='#bdbdbd'
+                            marginBottom={'30px'}
+                        >
+                            <CompanyIcon
+                                width='25'
+                                height='25'
+                                color='#fafafa'
+                            />
+                        </Flex>
+                        <Text mb={'0.5rem'} color={'#333'} fontWeight={'600'} fontSize={'1.125rem'}>{company.name}</Text>
+                    </CardBody>
+                </Card>)}
         </Flex>
     )
 }
